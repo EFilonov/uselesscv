485c2fce13be6779f74f96866aca21cb9ef25da3
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-08-12 23:45:21 +0300

    fix: header name bug

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 import { UserMenu } from '../UserMenu/UserMenu';
+import { useStore } from './../../store/zustandStore';
 import './Header.css';
 import { HeaderProps } from './Header.props.interface';
 
 export const Header: React.FC<HeaderProps> = ({ animate, handleDownload, handlePrint }) => {
+    const data = useStore((state) => state.data);
     return (
         <header className='header'>
             <div className='headerWrapper'>
-                <h1 className={`headerContent${animate ? ' animate' : ' hidden'}`}>LANDON HARPER</h1>
+                <h1 className={`headerContent${animate ? ' animate' : ' hidden'}`}>{data?.name}</h1>
                 <div className='headerBtns'>
                     <UserMenu handleDownload={handleDownload} handlePrint={handlePrint} />
                 </div>
