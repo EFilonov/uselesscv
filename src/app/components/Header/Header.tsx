b973842143f0e0da788a41641508de2a9797503f
Evgeniy Y. Filonov <evgeniyfilonov@gmail.com>
2025-08-14 20:04:51 +0300

    logo and README

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Image from 'next/image';
 
@@ -13,14 +13,8 @@ export const Header: React.FC<HeaderProps> = ({ animate, handleDownload, handleP
         <header className='header'>
             <div className='headerWrapper'>
                 <h1 className={`headerContent${animate ? ' animate' : ' hidden'}`}>{data?.name}</h1>
-                <Image
-                    src='./Logo.svg'
-                    alt='Logo'
-                    width={50}
-                    height={140}
-                    className={`headerLogo${animate ? ' animate' : ' hidden'}`}
-                    priority
-                />
+                <Image src='/Logo.svg' alt='Logo' width={140} height={40} className='logoLarge headerLogo' />
+                <Image src='/LogoSmall.svg' alt='Logo' width={50} height={24} className='logoSmall headerLogo' />
                 <div className='headerBtns'>
                     <UserMenu handleDownload={handleDownload} handlePrint={handlePrint} />
                 </div>
